f9a621f3d50aebe763a7c2d9827843ab417cfe6d
Victor Granados <victorgranadosp@gmail.com>
2024-08-22 18:01:21 +0200

    Finished init.

---
+++ src/simulator/integrator/__init__.py
@@ -0,0 +1,4 @@
+from .distributions import Distribution, Dirac, Uniform, BreitWigner
+from .integrator import MonteCarloIntegrator
+from .particles import Electron, LightQuarks, ZBoson
+from .squared_matrix_element import squared_matrix_element
