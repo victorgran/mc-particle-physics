9cfcc3c9b08eea563fc34506f4bce2ef524f57c8
Victor Granados <victorgranadosp@gmail.com>
2024-08-22 17:45:38 +0200

    Finished writing necessary constants.

---
+++ src/simulator/constants.py
@@ -0,0 +1,14 @@
+"""
+Numerical constants needed throughout the project.
+"""
+
+# Overall constants.
+alpha_QED = 1 / 129
+alpha_QCD_MZ = 0.118  # QCD coupling at the Z mass scale.
+QCD_colors = 3
+sin2w = 0.223  # Squared sine of the Weinberg angle.
+f_conv = 389379656.0  # Physical units conversion factor.
+kappa = 1 / (4 * sin2w * (1 - sin2w))  # Prefactor for chi 1 and 2.
+N_q = 5  # Number of light quark flavours.
+true_sigma_1a = 42_213.183064  # Fixed beam energy at Z boson squared mass.
+true_sigma_1c = 9_929.327294  # Uniform distribution for s.
